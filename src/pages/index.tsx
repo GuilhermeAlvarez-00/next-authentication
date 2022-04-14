f98abf112d3e5c068663d34f73d532589c0fc306
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-04-14 15:35:33 -0300

    criando formulario de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
+import { FormEvent, useState } from 'react'
+import styles from '../styles/Home.module.css'
+
 export default function Home(): JSX.Element {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      email,
+      password,
+    }
+
+    console.log(data)
+  }
+
   return (
-    <div>
-      <h2>Hello World</h2>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   )
 }
