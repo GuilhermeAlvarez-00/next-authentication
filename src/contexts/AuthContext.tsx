57435ba3c6924e247fe8da827c8bc670e41daacc
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-04-18 11:27:34 -0300

    recuperando estado de autenticacao

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,28 @@
-import { createContext, ReactElement, useContext } from 'react'
+import {
+  createContext,
+  ReactElement,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { setCookie, parseCookies } from 'nookies'
+import { api } from '../services/api'
+import Router from 'next/router'
 
 interface SignInCredentials {
   email: string
   password: string
 }
 
+interface User {
+  email: string
+  permissions: string[]
+  roles: string[]
+}
+
 interface AuthContextProps {
   signIn(credentials: SignInCredentials): Promise<void>
+  user: User
   isAuthenticated: boolean
 }
 
@@ -17,17 +33,55 @@ interface AuthProviderProps {
 const AuthContext = createContext({} as AuthContextProps)
 
 export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>()
   const isAuthenticated = false
 
+  useEffect(() => {
+    const { 'nexttest.token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then((response) => {
+        const { email, permissions, roles } = response.data
+        setUser({
+          email,
+          permissions,
+          roles,
+        })
+      })
+    }
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
-    console.log('context', {
-      email,
-      password,
-    })
+    try {
+      const response = await api.post('/sessions', { email, password })
+      const { token, refreshToken, permissions, roles } = response.data
+
+      setCookie(undefined, 'nexttest.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      })
+
+      setCookie(undefined, 'nexttest.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      })
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+      Router.push('/dashboard')
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, user, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
   )
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,18 @@
+import Router from 'next/router'
+import { useEffect } from 'react'
+import { useAuth } from '../contexts/AuthContext'
+import { api } from '../services/api'
+
 export default function Dashboard() {
+  const { user, isAuthenticated } = useAuth()
+
+  useEffect(() => {
+    api.get('/me').then((response) => console.log(response.data))
+  }, [])
+
   return (
     <div>
-      <h2>Dashboard</h2>
+      <h2>Dashboard: {user?.email}</h2>
     </div>
   )
 }
