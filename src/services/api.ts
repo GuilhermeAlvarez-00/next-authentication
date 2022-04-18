0b68256a916504817244c793b0f73baf56aecc55
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-04-18 15:15:54 -0300

    realizando refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -9,3 +9,38 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies['nexttest.token']}`,
   },
 })
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data.code === 'token.expired') {
+        cookies = parseCookies()
+
+        const { 'nexttest.refreshToken': refreshToken } = cookies
+
+        api.post('/refresh', { refreshToken }).then((response) => {
+          const { token } = response.data
+
+          setCookie(undefined, 'nexttest.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+          })
+
+          setCookie(
+            undefined,
+            'nexttest.refreshToken',
+            response.data.refreshToken,
+            {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+            }
+          )
+
+          api.defaults.headers['Authorization'] = `Bearer ${token}`
+        })
+      } else {
+      }
+    }
+  }
+)
