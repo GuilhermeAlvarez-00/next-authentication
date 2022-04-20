d9c593279f0765ba326ee715ed11af69112afbca
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-04-20 16:32:37 -0300

    redirecionamento pelo SSR

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -3,7 +3,8 @@ import {
   GetServerSidePropsContext,
   GetServerSidePropsResult,
 } from 'next'
-import { parseCookies } from 'nookies'
+import { destroyCookie, parseCookies } from 'nookies'
+import { AuthTokenError } from '../errors/AuthTokenError'
 
 export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
   return async (
@@ -20,6 +21,20 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
       }
     }
 
-    return await fn(ctx)
+    try {
+      return await fn(ctx)
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nexttest.token')
+        destroyCookie(ctx, 'nexttest.refreshToken')
+
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          },
+        }
+      }
+    }
   }
 }
