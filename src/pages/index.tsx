4c6bb95b0b358b1497530aa1066d2cb028f459bd
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-04-20 12:57:42 -0300

    validando visitante

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { parseCookies } from 'nookies'
 import { FormEvent, useState } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest'
 
 export default function Home(): JSX.Element {
   const [email, setEmail] = useState('')
@@ -40,19 +41,8 @@ export default function Home(): JSX.Element {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
-
-  if (cookies['nexttest.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      },
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {},
   }
-}
+})
