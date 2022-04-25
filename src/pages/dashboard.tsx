1fe9eeefde008e8e3202ea48a6ab144d960501f2
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-04-25 13:42:10 -0300

    criando componente de permissao

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import Router from 'next/router'
 import { destroyCookie, parseCookies } from 'nookies'
 import { useEffect } from 'react'
+import { Can } from '../components/Can'
 import { useAuth } from '../contexts/AuthContext'
 import { AuthTokenError } from '../errors/AuthTokenError'
 import { useCan } from '../hooks/useCan'
@@ -11,15 +12,13 @@ import { withSSRAuth } from '../utils/withSSRAuth'
 export default function Dashboard() {
   const { user } = useAuth()
 
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.list'],
-  })
-
   return (
     <div>
       <h2>Dashboard: {user?.email}</h2>
 
-      {userCanSeeMetrics && <div>Metricas</div>}
+      <Can permissions={['metric.list']}>
+        <div>Metricas</div>
+      </Can>
     </div>
   )
 }
